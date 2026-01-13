0d7b7d7fa4b01c672c67dcac19faacdea7d8edf3
Clement Raymond <chinexzy37@gmail.com>
2026-01-13 16:34:30 +0000

    removed comments

--- fundit-contracts/src/interfaces/ISavingsVault.sol
+++ fundit-contracts/src/interfaces/ISavingsVault.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-/**
- * @title ISavingsVault
- * @notice Interface for SavingsVault contract
- */
 interface ISavingsVault {
     function depositFlexible(uint256 amount) external;
     function depositTarget(uint256 amount, uint256 targetAmount) external;
